44b11d8d7169e2b17058354d4cad4af67b094db1
RyosukeSasaki0 <rsasaki@skylight.co.jp>
2021-05-28 20:30:26 +0900

    ボタンを押したらページ遷移する変更

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,42 @@ client.connect( function(err, client) {
   if (err) {
     console.log(err); //エラー時にコンソールに表示
   } else {
-    client.query('SELECT shuppatsu_nm FROM kotsuhi_memo', function (err, result) {  //第１引数にSQL
+    client.query('SELECT * FROM kotsuhi_memo', function (err, result) {  //第１引数にSQL
+      var month=[];
+      var date=[];
+      var shuppatsu=[];
+      var totyaku=[];
+      var keiyu=[];
+      var shudan=[];
+      var money=[];
+      var times=[];
+      var job=[];
+      var memo=[];
+      for(var i in result.rows){
+        month[i]=result.rows[i].memo_ymd.getMonth()+1;
+        date[i]=result.rows[i].memo_ymd.getdate();
+        shuppatsu[i]=result.rows[i].shuppatsu_nm;
+        totyaku[i]=result.rows[i].totyaku_nm;
+        keiyu[i]=result.rows[i].keiyu_nm;
+        shudan[i]=result.rows[i].shudan_nm;
+        money[i]=result.rows[i].memo_kingaku;
+        times[i]=result.rows[i].times;
+        job[i]=result.rows[i].job_memo;
+        memo[i]=result.rows[i].biko_memo;
+      }
       res.render('index', {
         title: '交通費メモ',
         section1:'ユーザー登録データ',
         section2:'交通費データ',
-        datas: result.rows[0].shuppatsu_nm, //引き出したデータ
+        date:month+'/'+date,
+        shuppatsu: shuppatsu, //引き出したデータ
+        totyaku:totyaku,
+        keiyu:keiyu,
+        shudan:shudan,
+        money:money,
+        times:times,
+        job:job,
+        memo:memo
       });
       console.log(result); //コンソール上での確認用なため、この1文は必須ではない。
     });
