3ca66c166085b6f52c23416423e16ab5d1c40726
chijiiwa-akimasa <achijiiwa@skylight.co.jp>
2021-05-31 13:26:01 +0900

    自分の作業ファイルからgithubへの移行

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// to use environment variables in .env file
-require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var patternRouter = require('./routes/pattern');
+var detailRouter = require('./routes/detail');
 
 var app = express();
 
@@ -24,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/pattern', patternRouter);
+app.use('/detail', detailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var{Client}=require('pg');  //データベースを使うための宣言
-const dbpassword = process.env.DBPW //DBを使うのに必要
-const apiKey = process.env.APIKEY //APIkeyを使うのに必要
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 
-  /* データベース呼び出しのための宣言（恐らくこれでuser_dataとkotsuhi_memoの両方使える） */
-  var client=new Client({
-    user:'postgres',
-    host:'localhost',
-    database:'ex_support',
-    password:dbpassword,
-    port:5432
-});
-/* ここでデータベースにアクセスする */
-client.connect( function(err, client) {
-  if (err) {
-    console.log(err); //エラー時にコンソールに表示
-  } else {
-    client.query('SELECT shuppatsu_nm FROM kotsuhi_memo', function (err, result) {  //第１引数にSQL
-      res.render('index', {
-        title: '交通費メモ',
-        section1:'ユーザー登録データ',
-        section2:'交通費データ',
-        datas: result.rows[0].shuppatsu_nm, //引き出したデータ
-      });
-      console.log(result); //コンソール上での確認用なため、この1文は必須ではない。
-    });
-  }
-});
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
