794465712ec6e051d108b35335235e56f63b73e4
teamb-sklt <84830697+teamb-sklt@users.noreply.github.com>
2021-05-31 14:53:17 +0900

    移動パターン登録のjs/ejs更新（5/31）

--- routes/pattern.js
+++ routes/pattern.js
@@ -6,40 +6,32 @@ const apiKey = process.env.APIKEY //APIkeyを使うのに必要
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 
-  res.render('pattern', { title: '移動パターン登録' });
+  // res.render('pattern', { title: '移動パターン登録' });
   var client=new Client({
     user:'postgres',
     host:'localhost',
     database:'ex_support',
-    password:'shoji2324', // ここのPWはHerokuにUP後変更
+    password:dbpassword,
     port:5432
   });
+// });
+
+
+
+/* 移動パターン表示 */
+client.connect( function(err, client) {
+  if (err) {
+    console.log(err); //エラー時にコンソールに表示
+  } else {
+    client.query('SELECT * FROM user_data', function (err, result) {  //第１引数にSQL
+      res.render('pattern', {
+        title: '移動パターン登録',
+        datas: result.rows, //引き出したデータ
+      });
+    });
+  }
 });
 
-
-
-router.post('/', function(req, res, next) {
-  var pattern_str = req.body["pattern"];
-  var origin_str = req.body["origin"];
-  var destination_str = req.body["destination"];
-  var money_str = req.body["money"];
-
-  var con = "tcp://postgres:shoji2324@localhost:5432/ex_support";
-  pg.connect(con, function(err, client) {
-      var qstr = "selcet from ex_support (pattern,origin,destination,money) values($1, $2, $3, $4);";
-      var query = client.query(qstr,[pattern_str, origin_str, destination_str, money_str]);
-      query.on('end', function(row,err) {
-          response.redirect("/");
-      });
-      query.on('error', function(error) {
-          console.log("ERROR!");
-          response.render('pattern', {
-              title: "ERROR",
-              data: null,
-              message: "ERROR is occured!"
-          });
-      });
-  });
 });
 
 module.exports = router;
