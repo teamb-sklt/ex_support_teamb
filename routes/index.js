e61c6c529cb79d1e8251ad749deaceeb0de08e4a
RyosukeSasaki0 <rsasaki@skylight.co.jp>
2021-05-31 15:11:41 +0900

    ページ移動機能を追加

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,8 @@ client.connect( function(err, client) {
     console.log(err); //エラー時にコンソールに表示
   } else {
     client.query('SELECT * FROM kotsuhi_memo', function (err, result) {  //第１引数にSQL
+      var id=[];
+      var date0=[];
       var month=[];
       var date=[];
       var shuppatsu=[];
@@ -31,8 +33,10 @@ client.connect( function(err, client) {
       var job=[];
       var memo=[];
       for(var i in result.rows){
-        month[i]=result.rows[i].memo_ymd.getMonth()+1;
-        date[i]=result.rows[i].memo_ymd.getdate();
+        id[i]=result.rows[i].memo_no;
+        date0[i]=result.rows[i].memo_ymd;
+        month[i]=date0[i].getMonth()+1;
+        date[i]=date0[i].getDate();
         shuppatsu[i]=result.rows[i].shuppatsu_nm;
         totyaku[i]=result.rows[i].totyaku_nm;
         keiyu[i]=result.rows[i].keiyu_nm;
@@ -46,6 +50,72 @@ client.connect( function(err, client) {
         title: '交通費メモ',
         section1:'ユーザー登録データ',
         section2:'交通費データ',
+        id:id,
+        date:month+'/'+date,
+        shuppatsu: shuppatsu, //引き出したデータ
+        totyaku:totyaku,
+        keiyu:keiyu,
+        shudan:shudan,
+        money:money,
+        times:times,
+        job:job,
+        memo:memo
+      });
+      console.log(result); //コンソール上での確認用なため、この1文は必須ではない。
+    });
+  }
+});
+
+});
+
+router.post('/',function(req,res,next){
+  var id1=req.body.id;
+  console.log(req.body);
+  var client=new Client({
+    user:'postgres',
+    host:'localhost',
+    database:'ex_support',
+    password:dbpassword,
+    port:5432
+});
+/* ここでデータベースにアクセスする */
+client.connect( function(err, client) {
+  // client.query("update kotsuhi_memo set times=times+1,biko_memo=biko_memo+"+date+" where id="+id1);
+  if (err) {
+    console.log(err); //エラー時にコンソールに表示
+  } else {
+    client.query('SELECT * FROM kotsuhi_memo', function (err, result) {  //第１引数にSQL
+      var id=[];
+      var date0=[];
+      var month=[];
+      var date=[];
+      var shuppatsu=[];
+      var totyaku=[];
+      var keiyu=[];
+      var shudan=[];
+      var money=[];
+      var times=[];
+      var job=[];
+      var memo=[];
+      for(var i in result.rows){
+        id[i]=result.rows[i].memo_no;
+        date0[i]=result.rows[i].memo_ymd;
+        month[i]=date0[i].getMonth()+1;
+        date[i]=date0[i].getDate();
+        shuppatsu[i]=result.rows[i].shuppatsu_nm;
+        totyaku[i]=result.rows[i].totyaku_nm;
+        keiyu[i]=result.rows[i].keiyu_nm;
+        shudan[i]=result.rows[i].shudan_nm;
+        money[i]=result.rows[i].memo_kingaku;
+        times[i]=result.rows[i].times;
+        job[i]=result.rows[i].job_memo;
+        memo[i]=result.rows[i].biko_memo;
+      }
+      res.render('index', {
+        title: '交通費メモ',
+        section1:'ユーザー登録データ',
+        section2:'交通費データ',
+        id:id,
         date:month+'/'+date,
         shuppatsu: shuppatsu, //引き出したデータ
         totyaku:totyaku,
